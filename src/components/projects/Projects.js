0ed9677a5a79f8ce274d8501850092dfacb2578c
Samadhan Patil <samadhanpatil0018@gmail.com>
2025-02-01 12:31:01 +0530

    Update Projects.js

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -345,24 +345,7 @@ const Projects = () => {
           </a>
         </div>
       </div>
-      <div
-        className="!col-full h-full opacity-25 md:!col-lgScreenRev md:h-auto md:opacity-100 shadow-aboutShadow transition-all duration-200 relative"
-        style={{ gridArea: '1 / 6 / -1 / -1' }}
-      >
-        <a
-          href="https://thirty-six-studios-git-main-samadhans-projects-d52f9515.vercel.app//"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="h-full"
-        >
-          <div className="group w-full relative max-w-[700px] h-full">
-            <div className="block relative w-full bg-themeBlue shadow-aboutShadow rounded-lg transition-all duration-500 mix-blend-screen group-hover:mix-blend-normal h-full z-[3]">
-              <img
-                className="w-auto md:w-full h-full min-h-[400px] bg-no-repeat rounded-lg relative object-cover transition-all duration-500 mix-blend-multiply grayscale contrast-[70%] brightness-50 md:brightness-95 group-hover:mix-blend-normal group-hover:filter-none opacity-85 md:opacity-100 z-[4]"
-                 // src={thirtysixImage}
-                alt=" Thirty Six studios home page"
-              />
-            </div>
+
           </div>
         </a>
       </div>
