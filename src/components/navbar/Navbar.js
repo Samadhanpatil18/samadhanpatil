f84582f926cd3e9887494ee0889139f0d30a571d
Samadhan Patil <samadhanpatil0018@gmail.com>
2025-04-21 17:32:25 +0530

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -166,7 +166,7 @@ const Navbar = () => {
 
           <div className="transition-all ease-in-out duration-300 hover:-translate-x-1 hover:-translate-y-1 mt-[15px]">
             <a
-              href="https://drive.google.com/file/d/12GLvmudaCgV1D6mmT6C-_Vc1ClhT4o5A/view?usp=drivesdk"
+              href="https://drive.google.com/file/d/19hQBLe0TDeFqaCWM36tt0ug6Wfsq67df/view?usp=drive_link"
               className="font-mono text-themeBlue bg-transparent border border-themeBlue rounded-[4px] py-[10px] px-4 text-[14px] transition-all ease-in-out duration-300 hover:shadow-buttonShadow"
               target="_blank"
               rel="noopener noreferrer"
